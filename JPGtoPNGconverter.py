7a0f651896bd5b5f17f755999670cbc27c2552ba
wayne chan <waynestock666@gmail.com>
2024-03-20 17:14:21 +0800

    change

--- JPGtoPNGconverter.py
+++ JPGtoPNGconverter.py
@@ -13,11 +13,11 @@ else:
     print(f'Folder {SAVE_folder} already exist')
 #loop through Pokedex
 for each_image in os.listdir(OLD_folder):
-    photo_name3 = each_image
+    photo_name5 = each_image
     if each_image.lower().endswith('.jpg'):
-        image_path = os.path.join(OLD_folder,photo_name3)
+        image_path = os.path.join(OLD_folder,photo_name5)
         img = Image.open(image_path)
-        save_path = os.path.join(SAVE_folder,photo_name3[:-4]+ '.png')
+        save_path = os.path.join(SAVE_folder,photo_name5[:-4]+ '.png')
         img.save(save_path)
     else:
         print('Support JPG file only')
